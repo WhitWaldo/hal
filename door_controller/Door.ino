c660afb0abbcb61c1d5d131aa7180288668adcfc
Daniel Nordness <Daniel0524@gmail.com>
2014-02-11 18:21:50 -0500

    removed bad code updated arduino code

--- door_controller/Door.ino
+++ door_controller/Door.ino
@@ -1,8 +1,13 @@
+#include <Servo.h> 
 #define DELAY 1000
-#define TRIGGER 72
-
+#define DOOR_TRIGGER 72
+#define UNLOCK_TRIGGER 73
+#define LOCK_TRIGGER 74
+#define ANGLE 90
 int incomingByte = 0;   // for incoming serial data
 int doorPin = 13;
+Servo myServo;
+int pos = 0;
 
 void openDoor() {
   digitalWrite(doorPin, HIGH);
@@ -10,22 +15,42 @@ void openDoor() {
   digitalWrite(doorPin, LOW);
 }
 
+void unlockDoor() {
+    for (pos = ANGLE; pos >= 1; pos -= 1) {
+        myServo.write(pos);
+        delay(15);
+    }
+}
+
+void lockDoor() {
+    for (pos = 0; pos < ANGLE; pos += 1) {
+        myServo.write(pos);
+        delay(15);
+    }
+}
+
 void setup() {
+  myServo.attach(9);
   pinMode(doorPin, OUTPUT);
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
-  // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-    if (incomingByte == TRIGGER)  {
-      openDoor();
+    // send data only when you receive data:
+    if (Serial.available() > 0) {
+        // read the incoming byte:
+        incomingByte = Serial.read();
+        // say what you got:
+        Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+        switch (incomingByte) {
+            case DOOR_TRIGGER:
+                openDoor();
+            case UNLOCK_TRIGGER:
+                unlockDoor();
+            case LOCK_TRIGGER:
+                lockDoor();
+        }
     }
-  }
 }
 
--- proj/tasks.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from proj.celery import app
-
-@app.task
-def add(x, y):
-        return x + y
