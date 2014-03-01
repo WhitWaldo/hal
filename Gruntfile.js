15911f9db99fe655380f6b2f6d28e589eb8bd1b6
Daniel Nordness <Daniel0524@gmail.com>
2014-03-01 16:04:57 -0500

    Added a number of things. SASS, JST, started working on the layout a bit.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+// Project configuration.
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+        html: {
+            files: [
+                'web/templates/*.html'
+            ],
+            tasks: ['jst']
+        },
+        css: {
+            files: [
+                '**/*.sass',
+                '**/*.scss'
+            ],
+            tasks: ['compass']
+        }
+    },
+    compass: {
+        dist: {
+            options: {
+                sassDir: 'web/sass',
+                cssDir:  'web/css',
+                outputStyle: 'compressed'
+            }
+        }
+    },
+    jst: {
+        compile: {
+            options: {
+                templateSettings: {}
+            },
+            files: {
+                "web/js/template.js" : ["web/templates/*.html"]
+            }
+        }
+    }
+});
+
+// Load the Grunt plugins.
+grunt.loadNpmTasks('grunt-contrib-compass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jst');
+// Register the default tasks.
+grunt.registerTask('build',['compass']);
+grunt.registerTask('default', ['watch']);
+};
