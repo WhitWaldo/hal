4db0fe56e7ebe7dc96ad138d9d9b8db4210b29da
Adam Edgett <AdamEdgett@gmail.com>
2014-02-28 22:48:11 -0500

    Uncommented accidental comment
    Oops!

--- door_controller/open_door.py
+++ door_controller/open_door.py
@@ -1,8 +1,8 @@
 import sys,serial
 
-#ARDUINO_LOCATION =  "/dev/ttyACM0"
-#BAUD_RATE = 9600
-#ser = serial.Serial(ARDUINO_LOCATION,BAUD_RATE)
+ARDUINO_LOCATION =  "/dev/ttyACM0"
+BAUD_RATE = 9600
+ser = serial.Serial(ARDUINO_LOCATION,BAUD_RATE)
 
 def send(char):
     try:
