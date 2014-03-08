208bf24c1117bd09c6279dc0070b514e50de64fc
Daniel Nordness <Daniel0524@gmail.com>
2014-03-08 16:35:07 -0500

    whoops

--- door_controller/open_door.py
+++ door_controller/open_door.py
@@ -2,15 +2,15 @@ import sys,serial
 
 ARDUINO_LOCATION =  "/dev/ttyACM0"
 BAUD_RATE = 9600
-# ser = serial.Serial(ARDUINO_LOCATION,BAUD_RATE)
+ser = serial.Serial(ARDUINO_LOCATION,BAUD_RATE)
 
 def send(char):
     print "sending" + char
-    # try:
+    try:
         # char is an arbitrarily defined etter code sent to the arduino
-        # ser.write(char)
-    # except:
-        # return "Unexpected error:", sys.exc_info()[0]
+        ser.write(char)
+    except:
+        return "Unexpected error:", sys.exc_info()[0]
 
 def buzz():
     send("H")
