3d126ab9e5ae9a09664f60f64e7a6bb80a8ecb91
Daniel Nordness <Daniel0524@gmail.com>
2014-03-08 19:37:23 -0500

    colors changed

--- static/web/js/template.js
+++ static/web/js/template.js
@@ -14,7 +14,7 @@ this["JST"]["static/web/templates/door.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<nav class="navbar navbar-translucent navbar-default navbar-fixed-top" role="navigation">\n<div class="container-fluid">\n  <a class="do-lights-page">\n    <div class="navbar-icon-box">\n      <i class="fa fa-3x fa-lightbulb-o navbar-icon"></i>\n    </div>\n  </a>\n</div>\n</nav>\n<div class="row">\n<a class="do-buzz-door"> \n  <div class="col-md-12 color-row color-row-1">\n      <i class="fa fa-5x fa-building-o"></i>\n  </div>\n</a>\n</div>\n<div class="row">\n<a class="do-unlock-door">\n  <div class="col-md-6 color-row color-row-2">\n    <i class="fa fa-5x fa-unlock-alt"></i>\n  </div>\n</a>\n<a class="do-lock-door">\n  <div class="col-md-6 color-row color-row-3">\n      <i class="fa fa-5x fa-lock"></i>\n  </div>\n</a>\n</div>';
+__p += '<nav class="navbar navbar-translucent navbar-default navbar-fixed-top" role="navigation">\n<div class="container-fluid">\n  <a class="do-lights-page">\n    <div class="navbar-icon-box">\n      <i class="fa fa-3x fa-lightbulb-o navbar-icon"></i>\n    </div>\n  </a>\n</div>\n</nav>\n<div class="row">\n<a class="do-buzz-door"> \n  <div class="col-md-12 color-row color-row-1">\n      <i class="fa fa-5x fa-building-o"></i>\n  </div>\n</a>\n</div>\n<div class="row">\n<a class="do-unlock-door">\n  <div class="col-md-6 color-row color-row-4">\n    <i class="fa fa-5x fa-unlock-alt"></i>\n  </div>\n</a>\n<a class="do-lock-door">\n  <div class="col-md-6 color-row color-row-3">\n      <i class="fa fa-5x fa-lock"></i>\n  </div>\n</a>\n</div>\n';
 
 }
 return __p
