9782554342e5283b4f21e32b4e9585a6922ca36a
Daniel Nordness <Daniel0524@gmail.com>
2014-05-15 09:30:24 -0400

    Added exception handling to wemo

--- wemo_controller/wemo.py
+++ wemo_controller/wemo.py
@@ -2,15 +2,26 @@ from ouimeaux.environment import Environment
 
 env = Environment()
 DISCOVER_DELAY = .5
+def start_and_discover(env, switch_name):
+    try:
+        env.start()
+        env.discover(seconds=DISCOVER_DELAY)
+        switch_list = env.list_switches()
+        if switch_name in switch_list:
+           switch = env.get_switch(switch_name)
+           return switch 
+        else:
+            return False
+    except:
+        print "An exception was raised trying to find the switch."
+        return False
 
 def on(switch_name, discover_delay = None):
     if discover_delay is None:
         return on(switch_name, DISCOVER_DELAY)
-    else: 
-        env.start() 
-        env.discover(seconds=discover_delay)
-        if switch_name in env.list_switches():
-            switch = env.get_switch(switch_name)
+    else:
+        switch = start_and_discover(env, switch_name)
+        if switch is not False: 
             switch.on()
             return str(switch.get_state()) 
         else:
@@ -20,10 +31,8 @@ def off(switch_name, discover_delay = None):
     if discover_delay is None:
         return off(switch_name, DISCOVER_DELAY)
     else:
-        env.start() 
-        env.discover(seconds=discover_delay)
-        if switch_name in env.list_switches():
-            switch = env.get_switch(switch_name)
+        switch = start_and_discover(env, switch_name)
+        if switch is not False: 
             switch.off()
             return str(switch.get_state()) 
         else:
@@ -33,11 +42,8 @@ def get_state(switch_name, discover_delay = None):
     if discover_delay is None:
         return get_state(switch_name, DISCOVER_DELAY)
     else:
-        env.start() 
-        env.discover(seconds=discover_delay)
-        if switch_name in env.list_switches():
-            switch = env.get_switch(switch_name)
-            switch.get_state()
+        switch = start_and_discover(env, switch_name)
+        if switch is not False: 
             return str(switch.get_state()) 
         else:
             return "No Switches Found With Name: " + switch_name;
