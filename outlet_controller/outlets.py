afe986341b225f5dbd0b31b59a94acceb518f35e
Daniel Nordness <Daniel0524@gmail.com>
2014-05-20 08:37:55 -0400

    took device_id out of vsc

--- outlet_controller/outlets.py
+++ outlet_controller/outlets.py
@@ -3,12 +3,12 @@ import settings
 # Turns Off the Outlet Specified
 def off(device):
     payload={'access_token' : settings.SPARK_ACCESS_TOKEN, 'params':'D'+ device +',LOW'}
-    url='https://api.spark.io/v1/devices/55ff71065075555345091787/digitalwrite'
+    url='https://api.spark.io/v1/devices/' + settings.DEVICE_ID +'/digitalwrite'
     requests.post(url, data=payload)
     return "true"
 #Turns On the Outlet Specified
 def on(device):
     payload={'access_token' : settings.SPARK_ACCESS_TOKEN, 'params':'D'+ device +',HIGH'}
-    url='https://api.spark.io/v1/devices/55ff71065075555345091787/digitalwrite'
+    url='https://api.spark.io/v1/devices/' + settings.DEVICE_ID + '/digitalwrite'
     requests.post(url, data=payload)
     return "true"
